cc33987f68ada470348c637c996acb5c3d41c71d
pipo <pipo@ubuntu.(none)>
2013-07-08 09:23:42 -0700

    commited by vajira

--- youthjob/forms.py
+++ youthjob/forms.py
@@ -5,8 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegistrationForm(UserCreationForm):
 	CHOICES = ((0, 'Applicant'), (1, 'Company'),)
 	email = forms.EmailField(required=True)
-	first_name = forms.CharField(required=True)
-	last_name = forms.CharField()
+	full_name = forms.CharField(required=True)
 	type = forms.TypedChoiceField(
 		coerce=lambda x: x == 'True', 
 		choices=((False, 'Applicant'), (True, 'Company')), 
@@ -15,14 +14,13 @@ class UserRegistrationForm(UserCreationForm):
 	
 	class Meta:
 		model = User
-		fields = ('type', 'username', 'first_name', 'last_name', 'email','password1', 'password2',)
+		fields = ('type', 'username', 'full_name', 'email','password1', 'password2',)
 
 	def save(self, commit=True):
 		user = super(UserCreationForm, self).save(commit=False)
 		user.set_password(self.cleaned_data["password1"])
 		user.email = self.cleaned_data['email']
-		user.first_name = self.cleaned_data['first_name']
-		user.last_name = self.cleaned_data['last_name']
+		user.first_name = self.cleaned_data['full_name']
 		user.is_staff = self.cleaned_data['type']
 		
 		if commit:
