c07bc8f0fc1d37ed3d916000e48efc98b43fdcee
ruwan <ruwan800@gmail.com>
2013-07-14 14:42:06 +0530

    Messed up with js.editor.html modified.

--- skills/urls.py
+++ skills/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     url(r'^list/$', 'skills.views.list_all'),
     url(r'^edit/$', 'skills.views.edit'),
     url(r'^add/$', 'skills.views.add'),
-    url(r'^search/(?P<text>...)', 'skills.views.search'),
+    url(r'^search/(?P<text>...|\w+)', 'skills.views.search'),
 )
   
