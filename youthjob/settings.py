68aceb117e4cf228c35f422aa0e6de70e0a8a08a
enadun <enadun@yahoo.com>
2013-07-16 16:22:31 +0530

    Make the absolute path & do some changes in cv preview html file

--- youthjob/settings.py
+++ youthjob/settings.py
@@ -65,7 +65,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/pipo/YouthJobs/youthjob/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
