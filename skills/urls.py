09e7508a67d3b58e2cb009f3a496df1b6a3c84a8
ruwan <ruwan800@gmail.com>
2013-07-18 15:09:57 +0530

    Now you can add skills

--- skills/urls.py
+++ skills/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     #list all companies
     url(r'^list/$', 'skills.views.list_all'),
     url(r'^edit/$', 'skills.views.edit'),
+    url(r'^adduserskills/$', 'skills.views.addUserSkills'),
     url(r'^add/$', 'skills.views.add'),
     url(r'^search/(?P<text>...|\w+)', 'skills.views.search'),
 )
--- skills/views.py
+++ skills/views.py
@@ -1,7 +1,10 @@
 from django.utils import simplejson
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
 from skills.models import Skill
+from vacancies.models import User_skills
+
 
 def search(request,text):
     skills = Skill.objects.filter(name__icontains=text.rstrip('/'))
@@ -15,3 +18,15 @@ def edit(request):
 def add(request):
     return render_to_response('hello.html')
 
+@csrf_exempt
+def addUserSkills(request):
+    response = request.POST
+    newSkills = [ str(x) for x in response.getlist('skills[]')]
+    print(newSkills)
+    currentUser = request.session.get('logged_user', False)
+    Addedskills = [x.skill_id.name for x in User_skills.objects.filter(user_id = currentUser )]
+    for newSkill in newSkills:
+        if not newSkill in Addedskills:
+            User_skills(user_id_id=currentUser, skill_id_id=Skill.objects.get(name = newSkill ).id).save()
+    return HttpResponse(simplejson.dumps(str(len(newSkills))+" skills added successfully"), mimetype='application/json')
+    
